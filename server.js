2656f709e7a2350ac1a1a9d7c7b76f9053075ce1
Your Name <you@example.com>
2025-08-03 21:57:05 -0300

    Alterações em rotas e modelos para adequação ao fronteend

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const HOST = process.env.HOST || '0.0.0.0';
+const HOST = process.env.HOST || 'localhost';
 
 // Middlewares
 app.use(cors());
